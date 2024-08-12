c29c24c6a9b549535b05ec922ca539f45e509d18
DanielWolfDigitalMedia <105422592+DanielWolfDigitalMedia@users.noreply.github.com>
2024-08-12 17:47:06 +0300

    despre-noi

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,22 +1,154 @@
 import Image from "next/image";
 import React from "react";
 
-const Logo = require("@/assets/images/logo-round.png");
-
 export default function page() {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <div className="mx-auto w-fit -mt-20">
-          <Image src={Logo} alt="Logo" width={200} height={200} />
-        </div>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Conținut în lucru
-        </h1>
-        <p className="mt-6 text-xl leading-7 text-gray-600">
-          Conținutul acestei pagini este în lucru. Vă rugăm să reveniți mai
-          târziu.
-        </p>
+    <main className="max-w-screen-xl mx-auto p-3 mb-10">
+      <h2 className="text-4xl font-black mt-8 mb-4 border-b-4 border-[#d6001d] w-fit">
+        Cine suntem noi?
+      </h2>
+
+      <p className="text-xl">
+        Perspectiva Comunistă este numele adoptat de către organizația noastră
+        cât și al organului nostru de presă, acest site. Noi suntem o
+        organizație Comunistă, Marxist-Leninist-Maoistă, Revoluționară al cărui
+        scop imediat este creerea unui Partid Proletar de Avangardă, iar scopul
+        publicației noastre de a fi vector al agitației și propagandei cu acest
+        scop final.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "Ne trebuie, înainte de toate, un ziar; fără el este imposibilă
+        desfășurarea sistematică a unei ample activități de propagandă și
+        agitație principială, consecventă, care constituie o sarcină permanentă
+        și principală a social-democrației, în general, și o sarcină deosebit de
+        actuală în momentul de față, când în straturile cele mai largi ale
+        populației s-a trezit interesul pentru politică, pentru problemele
+        socialismului."
+      </div>
+      <div className="text-xl italic text-right">
+        --Vladimir Lenin, De unde să începem
+      </div>
+
+      <h2 className="text-4xl font-black mt-8 mb-4 border-b-4 border-[#d6001d] w-fit">
+        Suntem Comuniști:
+      </h2>
+      <p className="text-xl">
+        Scopul nostru final este Comunismul, o societate lipsită de state, bani
+        sau clase, unde principiul de repartiție este "De la fiecare după
+        capacitate, fiecăruia după nevoie". Noi ne asociăm mișcării istorice ce
+        urmărește atingerea acestui stadiu în istorie prin construcția
+        socialistă.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "Comunismul pentru noi nu este o stare de lucruri care trebuie
+        stabilită, un ideal la care realitatea va trebui să se adapteze. Numim
+        comunismul adevărata mișcare care desființează starea actuală a
+        lucrurilor. Condițiile acestei mișcări rezultă din premisele existente
+        acum."
+      </div>
+      <div className="text-xl italic text-right">
+        --Karl Marx, Ideologia Germană
+      </div>
+      <h2 className="text-4xl font-black mt-8 mb-4 border-b-4 border-[#d6001d] w-fit">
+        Suntem Revoluționari:
+      </h2>
+      <p className="text-xl">
+        Noi realizăm că orânduirea actuală Capitalistă nu poate fi îmbunătățită,
+        remediată sau transformată gradual prin metode parlamentare, acest lucru
+        în cel mai bun caz ar aduce concensiuni temporare, pe care clasa
+        burgheză le poate retrage oricând. Noi milităm pentru distrugerea
+        completă a Mașinăriei Statului Burghez și înlocuirea ei cu Statul
+        Proletariatului.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "Dacă te uiți la ultimul capitol din Oprezecelea Brumaire, vei descoperi
+        că spun că următoarea încercare a revoluției franceze nu va mai fi, ca
+        până acum, de a transfera mașina birocratic-militară dintr-o mână în
+        alta, ci de a o distruge. Iar acest lucru este esențial pentru orice
+        adevărată revoluție populară de pe continent. Și asta încearcă eroicii
+        noștri tovarăși din Paris."
+      </div>
+      <div className="text-xl italic text-right">
+        --Karl Marx, Scrisoare către Kugelmann, Aprilie 1871
+      </div>
+      <h2 className="text-4xl font-black mt-8 mb-4 border-b-4 border-[#d6001d] w-fit">
+        Suntem Marxiști-Leniniști-Maoiști:
+      </h2>
+      <p className="text-xl">
+        Bazele teoretice ale noastre sunt cele stabilite de Karl Marx și
+        Friedrich Engels, adică suntem Marxiști. Acceptăm contribuțiile făcute
+        de Vladimir Lenin, anume, aplicarea Marxismului la perioada
+        Imperialismului și a Revoluției Proletare, și sinteza acestor
+        contribuții de Iosef Stalin, adică suntem Marxiști-Leniniști.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "Leninismul este marxismul epocii imperialismului și a revoluției
+        proletare. Mai exact, leninismul este teoria și tactica revoluției
+        proletare în general, teoria și tactica dictaturii proletariatului în
+        special."
+      </div>
+      <div className="text-xl italic text-right">
+        --Iosef Stalin, Bazele Leninismului
+      </div>
+      <p className="text-xl">
+        Ne opunem revizioniștilor din a doua jumătate a secolului trecut precum
+        N. Hrușcev, L. Brejnev și Deng Xiaoping, adică suntem Marxiști-Leniniști
+        Anti-Revizioniști. Aceptăm importantele contribuții ale lui Mao Zedong
+        în toate cele trei ramuri ale Marxismului, adică urmăm
+        Marxism-Leninism-Gândirea Mao Zedong. Și urmăm sinteza acestora drept al
+        treilea stadiu al Marxismului după cum a fost descris de RIM în 1995 în
+        numărul 20 al publicație A World to Win.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "În cursul revoluției chineze, Mao a dezvoltat marxismul-leninismul în
+        multe domenii importante. Dar în creuzetul Marii Revoluții Culturale
+        Proletare a făcut un salt ideologia noastră și a treia mare piatră de
+        hotar, marxismul-leninismul-maoismul, a apărut pe deplin. Din planul
+        superior al marxismului-leninismului-maoismului, comuniștii
+        revoluționari au putut înțelege și mai profund învățăturile marilor
+        lideri anteriori și într-adevăr chiar contribuțiile anterioare ale lui
+        Mao Tsetung au căpătat o semnificație mai profundă. Astăzi, fără maoism
+        nu poate exista marxism-leninism. Într-adevăr, a nega maoismul înseamnă
+        a nega însuși marxismul-leninismul."
+      </div>
+      <div className="text-xl italic text-right">
+        --RIM, Trăiască Marxism-Leninism-Maoism-ul (1993)
+      </div>
+      <h2 className="text-4xl font-black mt-8 mb-4 border-b-4 border-[#d6001d] w-fit">
+        Suntem un Precursor al Partidului Proletar de Avangardă:
+      </h2>
+      <p className="text-xl">
+        Noi realizăm că, pentru ca interesele proletariatului să fie
+        reprezentate și pentru ca acesta să își joace rolul istoric de a
+        construi Socialismul, este necesară trezirea elementelor cele mai
+        conștiente ale proletariatului și concentrarea lor într-un Partid
+        Proletar de Avangardă care să poată duce lupta de clasă. Spre acest scop
+        există Perspectiva Comunistă pentru a trezi și conecta aceste elemente.
+      </p>
+      <br />
+
+      <div className="text-xl italic">
+        "Comuniştii sînt, aşadar, din punct de vedere practic, partea cea mai
+        hotărîtă a partidelor muncitoreşti din toate ţările, partea care împinge
+        mereu înainte; din punct de vedere teoretic, faţă de restul masei
+        proletariatului ei au superioritatea de a înţelege limpede condiţiile,
+        mersul şi rezultatele generale ale mişcării proletare. Scopul imediat al
+        comuniştilor este acelaşi ca şi al tuturor celorlalte partide proletare:
+        constituirea proletariatului ca clasă, răsturnarea dominaţiei
+        burgheziei, cucerirea puterii politice de către proletariat."
+      </div>
+      <div className="text-xl italic text-right">
+        --Karl Marx și Friedrich Engels, Manifestul Partidului Comunist
       </div>
     </main>
   );
